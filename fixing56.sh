d1088b959a7b588d3ff63b8112d45799407a22b2
Firza Hadzami <firza.hdzami@gmail.com>
2025-04-29 13:35:47 +0700

    Create fixing56.sh

---
+++ fixing56.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+rm -f G.O.D/core/models/config_models.py
+
+cat > G.O.D/core/models/config_models.py <<EOL
+from dataclasses import dataclass
+
+
+@dataclass
+class BaseConfig:
+    wallet_name: str
+    hotkey_name: str
+    subtensor_network: str
+    netuid: int
+    env: str
+    subtensor_address: str | None = None  # Tetap di sini
+
+
+@dataclass
+class MinerConfig(BaseConfig):
+    wandb_token: str = ""
+    huggingface_username: str = ""
+    huggingface_token: str = ""
+    min_stake_threshold: str = ""
+    refresh_nodes: bool = False
+    is_validator: bool = False
+
+
+@dataclass
+class ValidatorConfig(BaseConfig):
+    postgres_user: str | None = None
+    postgres_password: str | None = None
+    postgres_db: str | None = None
+    postgres_host: str | None = None
+    postgres_port: str | None = None
+    s3_compatible_endpoint: str = ""
+    s3_compatible_access_key: str = ""
+    s3_compatible_secret_key: str = ""
+    s3_bucket_name: str = ""
+    frontend_api_key: str = ""
+    validator_port: str = ""
+    set_metagraph_weights: bool = False
+    gpu_ids: str = ""
+    gpu_server: str | None = None
+    localhost: bool = False
+    env_file: str = ".vali.env"
+    hf_datasets_trust_remote_code: bool = True  # Aku tambahkan type hint boolean
+    s3_region: str = "us-east-1"
+    refresh_nodes: bool = True
+    database_url: str | None = None
+    postgres_profile: str = "default"
+
+
+@dataclass
+class AuditorConfig(BaseConfig):
+    ...
+
+EOL
